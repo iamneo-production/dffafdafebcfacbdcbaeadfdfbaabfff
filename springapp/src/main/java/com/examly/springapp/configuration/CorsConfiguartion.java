90c84de510b78221c260e3a1cc90fc0743e0f712
Iamneo <techadmin@iamneo.ai>
2023-08-27 14:29:24 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguartion.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguartion.java
@@ -18,7 +18,7 @@ public class CorsConfiguartion implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**") // URL patterns to apply CORS configuration
-	            .allowedOrigins("https://8080-dffafdafebcfacbdcbaeadfdfbaabfff.premiumproject.examly.io/","http://localhost:9647") // Allowed origin URLs
+	            .allowedOrigins("https://8080-dffafdafebcfacbdcbaeadfdfbaabfff.premiumproject.examly.io/","http://localhost:9876") // Allowed origin URLs
 	            .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // Allowed HTTP methods
 	            .allowedHeaders("*")   // Allowed headers
             .allowCredentials(true);
